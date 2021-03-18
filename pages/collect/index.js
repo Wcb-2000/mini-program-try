92ec225caf279a30a1b199c9433f2a42740ee18c
Wcb-2000 <846583697@qq.com>
2021-03-18 23:58:11 +0800

    收藏夹交互的操作练习

--- pages/collect/index.js
+++ pages/collect/index.js
@@ -5,8 +5,46 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    collectList:[],
+    isData:false
   },
+
+//点击删除商品
+del: function (event) {
+  // splice(下标值，删除个数)
+  // var arr=[1,2,3,4,5,6]
+  // var res = arr.splice(3,1)
+  // console.log('arr==>',arr)
+  // console.log('res==>',res)
+
+  //获取下标值
+  var index = event.target.dataset.index
+  //获取缓存数据
+  var collectList = wx.getStorageSync('collectList')
+  var isData = true
+  //删除选中的商品下标值
+  collectList.splice(index, 1)
+  //判断收藏是否有商品,如果没有则隐藏数据页面
+  if(collectList.length==0){
+    isData=false
+  }
+  //修改data数据
+  //渲染界面
+  this.setData({
+    collectList,
+    isData
+  })
+  //修改缓存数据
+  wx.setStorageSync('collectList',collectList)
+  
+},
+
+//到小米商城点击事件
+toIndex: function () {
+  wx.switchTab({
+    url: '/pages/index/index',
+  })
+},
 
   /**
    * 生命周期函数--监听页面加载
@@ -26,7 +64,18 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+ //获取收藏缓存数据
+ var collectList = wx.getStorageSync('collectList')
+ //定义缓存状态
+ var isData = false
+ //判断收藏是否有数据
+ if (collectList.length != 0) {
+   isData = true
+ }
+ this.setData({
+   isData: isData,
+   collectList: collectList
+ })
   },
 
   /**
