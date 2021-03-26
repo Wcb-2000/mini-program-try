fbb129d3bbf9689118129a121e6b40f55b5ae20a
Wcb-2000 <846583697@qq.com>
2021-03-26 10:17:07 +0800

    订单代码优化，数据绑定，集合

--- pages/settlement/settlement.js
+++ pages/settlement/settlement.js
@@ -6,14 +6,14 @@ Page({
    */
   data: {
     //是否有地址
-    isAddress:true,
+    isAddress: true,
     //商品数据
-    settlementList:[],
+    settlementList: [],
     //商品总价
-    totalNum:1,
-    totalPrice:0,
+    totalNum: 1,
+    totalPrice: 0,
     //判断购买商品状态，1代表详情页，2代表购买页
-    id:1,
+    id: 1,
   },
 
   /**
@@ -22,29 +22,30 @@ Page({
   onLoad: function (options) {
     //console.log(options)
     var orderId = wx.getStorageSync('orderId')
-    if(orderId.length == 0){
+    if (orderId.length == 0) {
       wx.setStorageSync('orderId', 0)
     }
     this.setData({
-      id:options.id,
+      id: options.id,
       orderId
     })
   },
 
   //去付款的点击事件
-  toPay:function(){
-    var that =this
+  toPay: function () {
+    var that = this
     //先判断是否有地址
-    if(that.data.isAddress){  
+    if (that.data.isAddress) {
       //有地址
       //删除购物车中当前的商品 id=2
-      if(that.data.id == 2){
+      if (that.data.id == 2) {
         // console.log("从购物车中购买商品")
+
         var cartList = wx.getStorageSync('cartList')
         //判断商品是否选中购买
-        for(var i =cartList.length-1;i>=0;i--){
-          if(cartList[i].check){
-            cartList.splice(i,1)
+        for (var i = cartList.length - 1; i >= 0; i--) {
+          if (cartList[i].check) {
+            cartList.splice(i, 1)
           }
         }
         console.log(cartList)
@@ -53,46 +54,67 @@ Page({
       // console.log(that.data.id)
       //模态框
       wx.showModal({
-        title:'￥'+that.data.totalPrice,
-        content:'是否付款？',
-        success(res){
-          if(res.confirm){
+        title: '￥' + that.data.totalPrice,
+        content: '是否付款？',
+        success(res) {
+          if (res.confirm) {
             console.log('用户点击确定')
             //将当前数据添加到待收货，全部订单的缓存数据
             //定义订单号 orderId，每次购买商品都不同
             var settlementList = wx.getStorageSync('settlementList')
             var orderId = wx.getStorageSync('orderId')
-            // console.log(orderId)
-            for(var m=0;m<settlementList.length;m++){
-              console.log(settlementList[m])
-              //给每个商品添加订单号和按钮文本[确认收获]
-              orderId++
-              settlementList[m].orderId = orderId
-              settlementList[m].btn='确认收货'
-            }
-            // console.log(that.data.settlementList)
-            // console.log(new Date())
-          wx.setStorageSync('orderId', orderId)
-        }else if(res.cancel){
-          console.log('用户点击取消')
-          //删除购物车中当前的商品 id=2
-          //将当前数据添加到待付款，全部订单的缓存数据中
+            var received = wx.getStorageSync('receivedList')
+            var allOrder = wx.getStorageSync('allOrderList')
+            that.order('paymentList', received, orderId, '确认收获')
+            that.order('allOrderList', allOrder, orderId, '确认收获')
+          } else if (res.cancel) {
+            console.log('用户点击取消')
+            //将当前数据添加到待付款，全部订单的缓存数据中
+            var orderId = wx.getStorageSync('orderId')
+            var parment = wx.getStorageSync('paymentList')
+            var allOrder = wx.getStorageSync('allOrderList')
+            that.order('paymentList', payment, orderId, '去付款')
+            that.order('allOrderList', allOrder, orderId, '去付款')
+          }
         }
-      }
       })
-    }else{
+    } else {
       //没有地址
-     wx.showToast({
-       title: '请输入地址',
-       icon:'none'
-     })
+      wx.showToast({
+        title: '请输入地址',
+        icon: 'none'
+      })
     }
+  },
+
+  order: function (str, data, orderId, btn) {
+    //获取用户结算的缓存数据
+    var settlementList = wx.getStorageSync('settlementList')
+    //循环遍历数据
+    for (var m = 0; m < settlementList.length; m++) {
+      // console.log(settlementList[m])
+      //给每个商品添加订单号和按钮文本[确认收获]
+      orderId++
+      settlementList[m].orderId = orderId
+      settlementList[m].btn = btn
+      //判断订单页面是否有数据
+      if (data.length == 0) {
+        //如果没有则添加数据
+        data = [settlementList[m]]
+        // wx.setStorageSync(str, [settlementList[m]])
+      } else {
+        data.unshift(settlementList[m])
+      }
+      //修改缓存数据
+      wx.setStorageSync(str, data)
+    }
+    wx.setStorageSync('orderId',orderId)
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
@@ -102,12 +124,12 @@ Page({
     var good = wx.getStorageSync('settlementList')
     var totalNum = 0
     var totalPrice = 0
-    for(var i = 0;i<good.length;i++){
+    for (var i = 0; i < good.length; i++) {
       totalNum += good[i].num
       totalPrice += good[i].num * good[i].price
     }
     this.setData({
-      settlementList:good,
+      settlementList: good,
       totalNum,
       totalPrice
     })
