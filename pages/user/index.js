413f499840d2a4887cc6479c6c6f065b4ea3385f
Wcb-2000 <846583697@qq.com>
2021-04-02 10:43:47 +0800

    我的界面代码优化

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,49 +1,80 @@
-// pages/user/index.js
+// pages/my/my.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-   canIUse: true,
-   nickName:"",
-   userUrl:""
+     // 判断用户信息和授权按钮的显示隐藏
+     canIUse: true,
+     // 昵称
+     nickName: "",
+     // 头像
+     userUrl: "",
+     // 订单数组
+     orderList:[
+        {
+           img:"../../images/allorder.png",
+           text:"全部订单"
+        },
+        {
+           img:"../../images/payment.png",
+           text:"待付款"
+        },
+        {
+           img:"../../images/recevied.png",
+           text:"待收货"
+        }
+     ]
   },
-  bindGetUserInfo (e) {
-    console.log(e.detail.userInfo)
-    var nickName = e.detail.userInfo.nickName
-    var userUrl = e.detail.userInfo.avatarUrl
-    this.setData({
-      canIUse:false,
-      userUrl:userUrl,
-      nickName:nickName
-    })
+
+  // 点击跳转到订单页面
+  toOrder:function(event){
+     var index = event.target.dataset.index
+     wx.navigateTo({
+       url: '/pages/order/order?index='+index,
+     })
   },
+
+  // 授权登录的点击事件
+  bindGetUserInfo(e) {
+     console.log(e.detail.userInfo)
+     var nickName = e.detail.userInfo.nickName
+     var userUrl = e.detail.userInfo.avatarUrl
+     // 修改data里面的数据
+     this.setData({
+        canIUse: false,
+        userUrl: userUrl,
+        nickName: nickName
+     })
+  },
+
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var _this=this
+     var _this = this
      // 查看是否授权
      wx.getSetting({
-      success (res){
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-          wx.getUserInfo({
-            success: function(res) {
-              var nickName = res.userInfo.nickName
-              var userUrl = res.userInfo.avatarUrl
-              _this.setData({
-                canIUser: false,
-                userUrl:userUrl,
-                nickName:nickName
+        success(res) {
+           if (res.authSetting['scope.userInfo']) {
+              // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+              wx.getUserInfo({
+                 success: function (res) {
+                    var nickName = res.userInfo.nickName
+                    var userUrl = res.userInfo.avatarUrl
+                    // 修改data里面的数据
+                    _this.setData({
+                       canIUse: false,
+                       userUrl: userUrl,
+                       nickName: nickName
+                    })
+                 }
               })
-            }
-          })
+           }
         }
-      }
-    })
-
+     })
   },
 
   /**
@@ -56,9 +87,7 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function () {},
 
   /**
    * 生命周期函数--监听页面隐藏
